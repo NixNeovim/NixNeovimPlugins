d1b429613c65791dfb1a7fe35ca48176233039b4
Mitsuhiro Nakamura <m.nacamura@gmail.com>
2021-12-11 16:29:56 +0900

    chore: remove flake-compat

--- flake.nix
+++ flake.nix
@@ -4,10 +4,9 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    flake-compat = { url = "github:edolstra/flake-compat"; flake = false; };
   };
 
-  outputs = { self, nixpkgs, flake-utils, flake-compat }: {
+  outputs = { self, nixpkgs, flake-utils }: {
     overlay = import ./overlay.nix;
   } // (flake-utils.lib.eachDefaultSystem (system:
   let
