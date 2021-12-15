31b08e389037cdfd988e7eea5e1696c227a83b2b
Mitsuhiro Nakamura <mitsuhiro_nakamura@albert2005.co.jp>
2021-12-15 12:31:34 +0900

    fix: add dependencies of some plugins

--- overrides.nix
+++ overrides.nix
@@ -3,6 +3,25 @@ final: prev:
 let
   overrides = self: super:
   {
+    lspactions = super.lspactions.overrideAttrs (_: {
+      dependencies = with self; [
+        plenary-nvim
+        popup-nvim
+        astronauta-nvim
+      ];
+    });
+
+    telescope-heading-nvim = super.telescope-heading-nvim.overrideAttrs (_: {
+      dependencies = with self; [
+        telescope-nvim
+      ];
+    });
+
+    telescope-bibtex-nvim = super.telescope-bibtex-nvim.overrideAttrs (_: {
+      dependencies = with self; [
+        telescope-nvim
+      ];
+    });
   };
 in
 
