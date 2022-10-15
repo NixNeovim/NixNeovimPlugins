1a24cf5f9e3e648100bf1d002b751e1ba23805b1
Joscha Loos <j.loos@posteo.net>
2022-10-16 01:53:02 +0200

    feat: workflow for checking duplicates

--- bin/check-duplicates.sh
+++ bin/check-duplicates.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
-cat ./pkgs/vim-plugins.nix | grep -E "^  [a-zA-Z-]+ =" | sed -E 's/  ([a-zA-Z-]+) =.*$/\1/'  | sort | uniq -d
+plugins=$(cat ./pkgs/vim-plugins.nix | grep -E "^  [a-zA-Z-]+ =" | sed -E 's/  ([a-zA-Z-]+) =.*$/\1/' | sort)
+
+[ $(echo $plugins | uniq -d | wc -l) -gt 0 ] && echo $plugins | uniq -d && exit 1
