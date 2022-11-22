84f9a3df8488c77752bd5387c8ee29be5c5155a6
Joscha Loos <j.loos@posteo.net>
2022-11-22 15:14:39 +0100

    chore: adapt check-duplicates workflow

--- bin/check-duplicates.sh
+++ bin/check-duplicates.sh
@@ -3,4 +3,38 @@
 plugins="$(cat ./pkgs/vim-plugins.nix | grep -E "^  [a-zA-Z-]+ =" | sed -E 's/^  ([a-zA-Z-]+) =.*$/\1/' | sort)"
 count=$(echo "$plugins" | uniq -d | wc -l)
 
-[ $count -gt 0 ] && echo "$plugins" | uniq -d && exit 1 || echo "No duplicates found" && exit 0
+known_issues=$(gh issue list --state "open" --label "bot" --json "body" | jq -r ".[].body")
+
+if [ $count -gt 0 ]
+then
+    filtered_plugins=$(echo "$plugins" | uniq -d)
+
+    # iterate over plugins we found missing and
+    # compare them to all open issues.
+    # We no matching issue was found, we create a new one
+    for f in "$filtered_plugins"
+    do
+        found=false
+
+        for k in $known_issues
+        do
+            if [[ "$f" == "$k" ]]
+            then
+                found=true
+                break
+            fi
+        done
+
+        # test if matching issue was found
+        if ! $found
+        then
+            echo "Did not find an issue for $f. Creating a new one ..."
+            gh issue create --title "Detected broken plugin: $f" --label "bot" --body "$f"
+        else
+            echo "Issue for $f already exists"
+        fi
+
+    done
+else
+    echo "No duplicates found"
+fi
