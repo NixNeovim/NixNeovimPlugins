52b81421c9e5325c9339443eee66f8f2bbc35ab1
Joscha Loos <j.loos@posteo.net>
2023-07-10 01:27:26 +0200

    fix: add runtime dependencies

--- pkgs/update-vim-plugins.nix
+++ pkgs/update-vim-plugins.nix
@@ -1,4 +1,4 @@
-{ mkPoetryApplication, nix, nix-prefetch-git, alejandra, pkgs }:
+{ mkPoetryApplication, nix, nix-prefetch-git, alejandra, pkgs, lib }:
 
 mkPoetryApplication {
   projectDir = ../bin;
@@ -7,6 +7,12 @@ mkPoetryApplication {
     nix-prefetch-git
     alejandra
   ];
+
+  postFixup = ''
+      wrapProgram $out/bin/update-vim-plugins \
+        --prefix PATH : ${lib.makeBinPath [ pkgs.alejandra ]}
+    '';
+
   # FIX:
   # checkPhase = "${pkgs.python310Packages.pytest}/bin/pytest --cov update_vim_plugins --cov-report term-missing:skip-covered --cov-fail-under 50 update_vim_plugins/tests";
 }
