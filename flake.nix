9d97f256299e2e643a2ab4c21e44d197eff9e46e
Joscha Loos <j.loos@posteo.net>
2023-07-12 22:55:07 +0200

    refactor: clean up flake

--- flake.nix
+++ flake.nix
@@ -12,34 +12,62 @@
     overlays.default = import ./overlay.nix;
   } // (flake-utils.lib.eachDefaultSystem (system:
   let
+
+    inherit (flake-utils.lib)
+      filterPackages
+      mkApp;
+
+    inherit (pkgs)
+      callPackage
+      runCommandNoCC;
+
+    inherit (pkgs.lib)
+      mapAttrsToList;
+
+    inherit (poetry2nix.legacyPackages.${system})
+      mkPoetryApplication;
+
+
     pkgs = import nixpkgs {
       inherit system;
       overlays = [ self.overlays.default ];
     };
 
-    update-vim-plugins = pkgs.callPackage ./pkgs/update-vim-plugins.nix { inherit (poetry2nix.legacyPackages.${system}) mkPoetryApplication; };
+    update-vim-plugins = callPackage ./pkgs/update-vim-plugins.nix { inherit mkPoetryApplication; };
 
-    check-missing-licenses = let
-      hasLicense = pkg:
+    # cheks if a plugin has a license
+    hasLicense = pkg:
       let
+
         warn = x: nixpkgs.lib.warn x x;
-        msg = if builtins.hasAttr "license" pkg.meta
-        then "${pkg.name} has license"
-        else warn "${pkg.name} has no license";
+
+        msg =
+          if builtins.hasAttr "license" pkg.meta then
+            "${pkg.name} has license"
+          else
+            warn "${pkg.name} has no license";
+
         msg' = nixpkgs.lib.replaceStrings [" "] ["-"] msg;
-      in
-      pkgs.runCommandNoCC msg' {} "echo : > $out";
-    in
-    pkgs.runCommandNoCC "check-missing-licenses" {
-      buildInputs = nixpkgs.lib.mapAttrsToList
-      (_: pkg: hasLicense pkg)
-      self.packages.${system};
-    } "echo : > $out";
+
+      in runCommandNoCC msg' {} "echo : > $out";
+
+    # function to check lisece for all packages
+    check-missing-licenses =
+      let
+        buildInputs =
+          mapAttrsToList
+            (_: pkg: hasLicense pkg)
+            self.packages.${system};
+      in runCommandNoCC
+          "check-missing-licenses"
+          { inherit buildInputs; }
+          "echo : > $out";
+
   in {
-    packages = flake-utils.lib.filterPackages system pkgs.vimExtraPlugins;
+    packages = filterPackages system pkgs.vimExtraPlugins;
 
     apps = {
-      update-vim-plugins = flake-utils.lib.mkApp {
+      update-vim-plugins = mkApp {
         drv = update-vim-plugins;
       };
     };
