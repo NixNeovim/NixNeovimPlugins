4ec3336f968bc3d429ec78d0e7cc6060bccd6375
Joscha Loos <j.loos@posteo.net>
2023-07-17 17:34:15 +0200

    fix: plugins.md

--- bin/update_vim_plugins/update.py
+++ bin/update_vim_plugins/update.py
@@ -101,7 +101,7 @@ class UpdateCommand(Command):
 
         self.line(f"<info>Updating plugins.md</info>")
 
-        header = "| Repo | Last Update | Nix package name | Last checked |\n| :- | :- | :- |\n"
+        header = "| Repo | Last Update | Nix package name | Last checked |\n|:---|:---|:---|:---|\n"
 
         with open(PLUGINS_LIST_FILE, "w") as file:
             file.write(header)
