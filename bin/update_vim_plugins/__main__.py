abf4a32f44c88b7e0bad274c7bed343a7c93e5b5
Joscha Loos <j.loos@posteo.net>
2023-07-18 14:49:41 +0200

    feat: cleanup manifest command

--- bin/update_vim_plugins/__main__.py
+++ bin/update_vim_plugins/__main__.py
@@ -1,11 +1,13 @@
 from cleo.application import Application
 
 from .update import UpdateCommand
+from .cleanup import CleanUpCommand
 
 
 def main():
     application = Application()
     application.add(UpdateCommand())
+    application.add(CleanUpCommand())
     application.run()
 
 
