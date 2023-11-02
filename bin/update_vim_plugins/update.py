80489972b2d60c9fa9d68b65534d3d4d8323ba4a
Joscha Loos <j.loos@posteo.net>
2023-11-02 15:51:25 +0100

    feat: add counter to plugins.md

--- bin/update_vim_plugins/update.py
+++ bin/update_vim_plugins/update.py
@@ -92,7 +92,7 @@ class UpdateCommand(Command):
 
         self.line(f"<info>Updating plugins.md</info>")
 
-        header = "| Repo | Last Update | Nix package name | Last checked |\n|:---|:---|:---|:---|\n"
+        header = f" - Plugin count: {len(plugins)}\n\n| Repo | Last Update | Nix package name | Last checked |\n|:---|:---|:---|:---|\n"
 
         with open(PLUGINS_LIST_FILE, "w") as file:
             file.write(header)
